5f21374a7d8ad3acc5d691b5a5050bb259520f1c
AnhThu1377 <nguyenthuyanhthu1377@gmail.com>
2025-02-28 13:57:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+funtion login()
+{
+  let a= username.value
+  let b= Password.value
+  if (a=="abc" && == "123")
+  {
+    window.open("home.html")
+    document.getElementById(compiler).style.display="b"
+  }
+  else
+    document.write("không hợp lệ")
+}
